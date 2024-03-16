a8a8bf0eb9b2bd73b15be7329b76ce2d4eb34bc6
Del Rockie <160012732+delrockie@users.noreply.github.com>
2024-03-16 11:06:30 +0300

    Add files via upload

---
+++ dartvariables.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Geofrey Mumo';
+  int age = 18;
+  String school = 'Power Learn Project';
+  String hobby = 'Gaming';
+
+  print(
+      '$name is $age years old, attending $school, and enjoys $hobby as a hobby.');
+}
